3ff309b90c005a8a9e011ad9bc677006e3d910ee
dannylessio <danny.lessio@gmail.com>
2016-02-29 18:17:52 +0100

    Fixed auto_build_RTK.sh

--- auto_build_RTK.sh
+++ auto_build_RTK.sh
@@ -2,31 +2,37 @@
 
 # Pick up the number of processors
 echo "Insert the number of your processor's cores followed by [ ENTER ]"
-read core
+read n_of_cores
 
-if [ ! -d "$ITK" ]; then
-	# Control will enter here if $DIRECTORY doesn't exist.
+if [ ! -d "./ITK" ]; then
 	git clone git://itk.org/ITK.git
-	cd ITK
+fi
+
+if [ ! -d "./ITK-bin" ]; then
+	mkdir ITK-bin
+	cd ITK-bin
+	cmake -DModule_ITKReview=ON -DITK_USE_FFTWD=ON -DITK_USE_FFTWF=ON -DBUILD_DOCUMENTATION=OFF -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF ../ITK
+	make -j $n_of_cores
+	cd..
+fi
+
+if [ ! -d "./RTK" ]; then
+	git clone git://github.com/SimonRit/RTK.git
+fi
+
+if [ ! -d "./RTK-bin" ]; then
+	# Getting the full path of ITK-bin
+	cd ITK-bin
+	path_of_ITK=$(pwd)
+	cd ..
+
+	# Compile RTK
+	mkdir RTK-bin
+	cd RTK-bin
+	cmake -DITK_DIR=$path_of_ITK -DCMAKE_BUILD_TYPE=Release ../RTK
+	make -j $n_of_cores
 	cd ..
 fi
 
-if [ -d "$ITK-bin" ]; then
-	# Control will enter here if $DIRECTORY exists.
-	echo "Removing ITK-bin"
-fi
 
-mkdir ITK-bin
-cd ITK-bin
-cmake -DModule_ITKReview=ON -DITK_USE_FFTWD=ON -DITK_USE_FFTWF=ON -DBUILD_DOCUMENTATION=OFF -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF ../ITK
-make -j $core
-path_of_ITK=pwd
-cd ..
-
-git clone git://github.com/SimonRit/RTK.git
-mkdir RTK-bin
-cd RTK-bin
-cmake -DITK_DIR=$path_of_ITK -DCMAKE_BUILD_TYPE=Release ../RTK
-make -j $core
-cd ..
 
